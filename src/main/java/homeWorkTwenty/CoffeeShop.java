6cb1bd74f1b5ebabb0b854a67a28576b8a3a7696
YuliyaChuyeva <yuliachueva@icloud.com>
2025-06-10 09:31:08 +0300

    lesson20

--- src/main/java/homeWorkTwenty/CoffeeShop.java
+++ src/main/java/homeWorkTwenty/CoffeeShop.java
@@ -4,29 +4,36 @@ import java.util.Scanner;
 
 public class CoffeeShop {
     public static void main(String[] args) {
-        System.out.println("Введите кофе на выбор: CAPPUCCINO,ESPRESSO,LATTE");
         Scanner scanner = new Scanner(System.in);
-        String vybor = scanner.nextLine();
-        System.out.println("Введите количество чашек");
-        int kolichestvo = scanner.nextInt();
-        CoffeeType coffeeType = CoffeeType.valueOf(vybor);
-        int totalCost = coffeeType.getPrice() * kolichestvo;
-        switch (coffeeType) {
-            case CAPPUCCINO:
-                System.out.println(String.format("Ваш напиток CAPUCCINO:%s,в количестве %d шт." +
-                        " Итоговая стоимость %d рублей", coffeeType.getDescription(), kolichestvo, totalCost));
-                break;
-            case ESPRESSO:
-                System.out.println(String.format("Ваш напиток ESPRESSO:%s,в количестве %d шт." +
-                        " Итоговая стоимость %d рублей", coffeeType.getDescription(), kolichestvo, totalCost));
-                break;
-            case LATTE:
-                System.out.println(String.format("Ваш напиток LATTE:%s,в количестве %d шт." +
-                        " Итоговая стоимость %d рублей", coffeeType.getDescription(), kolichestvo, totalCost));
-                break;
-            default:
-                System.out.println("Не верный выбор.");
-                return;
+        System.out.println("Введите кофе на выбор: CAPPUCCINO,ESPRESSO,LATTE");
+        CoffeeType coffeeType = null;
+        while (coffeeType == null) {
+            String choice = scanner.nextLine().toUpperCase();
+            try {
+                coffeeType = CoffeeType.valueOf(choice);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Вы введи неверный вариант, попробуйте снова.");
+            }
         }
+
+        System.out.println("Введите количество чашек");
+        int kolichestvo = 0;
+        while (kolichestvo <= 0) {
+            try {
+                kolichestvo = scanner.nextInt();
+                if (kolichestvo <= 0) {
+                    System.out.println("Количество должно быть положительным.");
+                }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        int totalCost = coffeeType.getPrice() * kolichestvo;
+        getInfo(coffeeType, kolichestvo, totalCost);
+    }
+
+    private static void getInfo(CoffeeType coffeeType, int kolichestvo, int totalCost) {
+        System.out.println(String.format("Ваш напиток:%s.\nОбъем напитка:%s.\nВ количестве %d шт." +
+                " \nИтоговая стоимость %d рублей.", coffeeType.getName(), coffeeType.getDescription(), kolichestvo, totalCost));
     }
 }
