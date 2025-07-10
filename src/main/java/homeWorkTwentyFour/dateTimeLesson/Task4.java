2e7390159bb958b366729ad3f471ed9dc5bc9e5e
YuliyaChuyeva <yuliachueva@icloud.com>
2025-07-10 09:16:24 +0300

    changes method name

--- src/main/java/homeWorkTwentyFour/dateTimeLesson/Task4.java
+++ src/main/java/homeWorkTwentyFour/dateTimeLesson/Task4.java
@@ -7,13 +7,13 @@ public class Task4 {
     public static void main(String[] args) {
         ZoneId london = ZoneId.of("Europe/London");
         ZoneId moscow = ZoneId.of("Europe/Moscow");
-        myMethod(london, moscow);
+        getTime(london, moscow);
         ZoneId utc = ZoneId.of("UTC");
         ZoneId tokyo = ZoneId.of("Asia/Tokyo");
-        myMethod(utc, tokyo);
+        getTime(utc, tokyo);
     }
 
-    public static void myMethod(ZoneId zoneId, ZoneId zoneId2) {
+    public static void getTime(ZoneId zoneId, ZoneId zoneId2) {
         int firstCountry = ZonedDateTime.now(zoneId).getOffset().getTotalSeconds();
         int secondCountry = ZonedDateTime.now(zoneId2).getOffset().getTotalSeconds();
         long different = Math.abs(firstCountry - secondCountry) / 3600;
